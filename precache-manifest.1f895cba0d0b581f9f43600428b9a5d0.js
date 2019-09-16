a7f96cc5a04d9223031caf085054c737fb6bdf4e
fangnx <fangnaxin@gmail.com>
2019-09-15 23:40:19 -0400

    Updates

---
+++ precache-manifest.1f895cba0d0b581f9f43600428b9a5d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd59eb73f60d8d5a5aa88b45cde0abc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67412c6d76231e4a2aca",
+    "url": "/static/css/main.a5f650a7.chunk.css"
+  },
+  {
+    "revision": "223cb93119ae6b662a6d",
+    "url": "/static/js/2.f6a9a756.chunk.js"
+  },
+  {
+    "revision": "67412c6d76231e4a2aca",
+    "url": "/static/js/main.1b3e147f.chunk.js"
+  },
+  {
+    "revision": "d833a6b4690df617695e",
+    "url": "/static/js/runtime~main.bb1db64d.js"
+  }
+]);
